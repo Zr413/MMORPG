2e4516cb7bcdecb24756a68c04685e9a9f707021
Zr413 <z0rin.13@yandex.ru>
2023-12-18 10:45:50 -0700

    Sending notifications by mail to the author of the review, in case of passing moderation and approval

--- guild/signals.py
+++ guild/signals.py
@@ -1,5 +1,5 @@
 from django.core.mail import send_mail
-from django.db.models.signals import post_save, pre_delete, pre_save, post_delete, m2m_changed
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.shortcuts import redirect
 
